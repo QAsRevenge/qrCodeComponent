216dd6dda8cea4a8ada475f1d94996bc34a384d1
QAsRevenge <hampus.e.molin@gmail.com>
2023-02-12 17:59:53 +0100

    first commit

---
+++ qrCodeComponent/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return <main className="modal">
+    <section className="qrContainer">
+      <img src="./public/images/image-qr-code.png" alt="" />
+    </section>
+    <section className="titleAndDescription">
+    <h1 className="title">Improve your front-end skills by building projects</h1>
+      <p className="description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </section>
+  </main>
+}
+
+export default App
